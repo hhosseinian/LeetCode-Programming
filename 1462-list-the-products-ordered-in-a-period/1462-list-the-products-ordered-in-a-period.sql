9b98fff42baf9a874ff18f10eeee2349b09a239e
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-09-11 03:48:57 -0400

    Time: 826 ms (22.26%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT P.product_name, SUM(O.unit) AS unit
+FROM Orders O
+LEFT JOIN Products P ON P.product_id = O.product_id
+WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY P.product_name
+HAVING SUM(O.unit)>=100;
