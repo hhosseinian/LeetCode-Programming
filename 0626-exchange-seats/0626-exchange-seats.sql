317a8a1855fe9752e818048e80c93772964e920a
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-09-16 01:19:00 -0400

    Time: 286 ms (93.33%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT (CASE WHEN id % 2 =1 and id = (SELECT max(id) FROM SEAT) THEN id
+    WHEN id % 2 =1 THEN id+1 
+WHEN id % 2 = 0 THEN id-1 END) AS id, student
+FROM Seat
+ORDER BY id
