825199d141fb013a752b407eb50336f134c91118
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-06 02:18:53 -0400

    Time: 65 ms (10.87%), Space: 16.5 MB (79.53%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+        for i in range(2,n+1):
+            for x in range(1,i):
+                if i%x ==0 and not dp[i-x]:
+                    dp[i]=True
+                    break
+        return dp[n]
