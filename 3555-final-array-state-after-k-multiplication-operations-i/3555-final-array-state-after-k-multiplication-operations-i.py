3ef4cfb25af06836f73acb6582e2b0db990c5720
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-05 21:45:50 -0400

    Time: 62 ms (34.35%), Space: 16.5 MB (81.29%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -7,9 +7,10 @@ class Solution:
         for _ in range(k):
             (x,inx)= heapq.heappop(min_heap)
             heapq.heappush(min_heap,(x*multiplier,inx))
-        results = [0]*len(nums)
-        while min_heap:
-            (x,inx)= heapq.heappop(min_heap)
-            results[inx] = x
-        return results
+            nums[inx] *=multiplier
+        #results = [0]*len(nums)
+        #while min_heap:
+        #    (x,inx)= heapq.heappop(min_heap)
+        #    results[inx] = x
+        return nums
         
