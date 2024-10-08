566a9aaf9497b446126cddeba13b9b028611764e
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-07 23:51:58 -0400

    Time: 435 ms (35.96%), Space: 18.5 MB (24.95%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,19 +2,26 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0]) if rows>=1 else 0
-        premetere = 0
+        perimeter = 0
+        def dfs(i,j):
+            nonlocal perimeter
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]
+            for dr,dc in directions:
+                nr,nc = i+dr,j+dc
+                if nr<0 or nr>=rows or nc<0 or nc>=cols or grid[nr][nc] ==0:
+                    perimeter+=1
+                elif grid[nr][nc] ==1:
+                    grid[nr][nc] =2
+                    dfs(nr,nc)
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
-                    if i == 0 or grid[i-1][j] == 0:
-                        premetere+=1
-                    if i == rows-1 or grid[i+1][j] == 0:
-                        premetere+=1
-                    if j == 0 or grid[i][j-1] == 0:
-                        premetere+=1
-                    if j == cols-1 or grid[i][j+1] == 0:
-                        premetere+=1
-        return premetere
+                if grid[i][j] ==1:
+                    grid[i][j] =2
+                    dfs(i,j)
+                    return perimeter
+        return perimeter
+                
+
 
 
         
