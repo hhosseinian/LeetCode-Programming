ad9c19b0784ebb54b9df5990079cb03570ecffea
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-10 02:22:00 -0400

    Time: 37 ms (38.01%), Space: 16.6 MB (39.52%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = deque()
+        current_num = 0
+        current_str = []
+        for char in s:
+            if char.isdigit():
+                current_num = 10*current_num + int(char)
+            elif char == '[':
+                stack.append((current_str,current_num))
+                current_num = 0
+                current_str = []
+            elif char == ']':
+                last_str,last_num =stack.pop()
+                current_str = last_str + current_str*last_num
+            else:
+                current_str.append(char)
+        return ''.join(current_str)
+
+
+
