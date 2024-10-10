573633acc893112b48467ad1d2f314cfff74c929
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-10 01:31:52 -0400

    Time: 125 ms (84.86%), Space: 17.9 MB (50.26%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,12 +1,14 @@
-from collections import defaultdict
+from collections import Counter
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        if len(word1) !=len(word2):
+        if len(word1) != len(word2):
             return False
-        freq_hashmap1 = defaultdict(int)
-        freq_hashmap2 = defaultdict(int)
-        for letter1 in word1:
-            freq_hashmap1[letter1]+=1
-        for letter2 in word2:
-            freq_hashmap2[letter2]+=1
-        return set(freq_hashmap1.keys()) == set(freq_hashmap2.keys()) and (sorted(freq_hashmap1.values())==sorted(freq_hashmap2.values()))
+        
+        # Use Counter to count frequencies directly
+        freq_counter1 = Counter(word1)
+        freq_counter2 = Counter(word2)
+        
+        # Compare the unique characters and their frequencies
+        return (freq_counter1.keys() == freq_counter2.keys() and 
+                sorted(freq_counter1.values()) == sorted(freq_counter2.values()))
