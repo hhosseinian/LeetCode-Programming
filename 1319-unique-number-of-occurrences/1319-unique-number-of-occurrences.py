3d27e4ad0432867ed0145e46d3a9e0b561ce133e
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-10 01:19:58 -0400

    Time: 35 ms (90.06%), Space: 16.5 MB (89.29%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,11 +1,9 @@
+from collections import defaultdict
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        hashmap_freq = {}
+        hashmap_freq = defaultdict(int)
         for a in arr:
-            if a in hashmap_freq:
                 hashmap_freq[a]+=1
-            else:
-                hashmap_freq[a] = 1
         checked = set()
         for v in hashmap_freq.values():
             if v in checked:
