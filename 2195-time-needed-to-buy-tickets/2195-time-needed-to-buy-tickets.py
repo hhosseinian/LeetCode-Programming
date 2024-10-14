a6c9eca7c395bfb1ba0ef35381263c5a668bded8
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-14 14:05:50 -0400

    Time: 50 ms (25.81%), Space: 16.7 MB (5.94%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -3,8 +3,8 @@ class Solution:
         result = 0
         n=len(tickets)
         ptr = 0
-        while tickets[k]!=0:
-            if tickets[ptr%n]!=0:
+        while tickets[k]>0:
+            if tickets[ptr%n]>0:
                 tickets[ptr%n]-=1
                 result+=1
             ptr+=1
