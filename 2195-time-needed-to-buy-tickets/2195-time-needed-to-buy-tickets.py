3d73ab1fa9fc31d620754b11adfa9c705c5ed8bf
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-14 14:25:19 -0400

    Time: 38 ms (76.03%), Space: 16.5 MB (33.2%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -3,10 +3,9 @@ class Solution:
         total_time = 0
         ticket_needed = tickets[k]
         n=len(tickets)
-        for i in range(n):
-            if i<=k:
+        for i in range(k+1):
                 total_time += min(ticket_needed,tickets[i])
-            else:
+        for i in range(k+1,n):
                 total_time += min(ticket_needed-1,tickets[i])
         return total_time
 
