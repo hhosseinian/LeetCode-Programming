b077541045eede7ed22a3f9bfaf4e2ea5796193c
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-16 12:45:40 -0400

    Time: 33 ms (75.35%), Space: 16.5 MB (30.96%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -2,6 +2,5 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
         for char in columnTitle:
-            result*=26
-            result += ord(char)-ord('A')+1
+            result = result*26+ ord(char)-ord('A')+1
         return result
