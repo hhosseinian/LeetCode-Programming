1b993a8423f931b899f6ee748f1656e3855bb944
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-15 23:35:37 -0400

    Time: 27 ms (97.25%), Space: 16.6 MB (73.76%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,19 +1,14 @@
 from collections import Counter
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        result = 0
         char_freq = Counter(s)
+        result = 0
         has_odd = False
-        for key,val in char_freq.items():
-            if val%2 == 0:
-                result+=val
-            elif val%2 == 1:
-                result+=val-1
+        for count in char_freq.values():
+            result += count // 2 * 2  # Add the largest even part
+            if count % 2 == 1:
                 has_odd = True
         if has_odd:
-            result+= 1
+            result+=1
         return result
-
-
-
-
