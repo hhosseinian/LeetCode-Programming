e7393ca19525e069050a908d0e436b642730e785
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-16 14:25:30 -0400

    Time: 33 ms (68.03%), Space: 16.5 MB (73.59%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -3,8 +3,9 @@ class Solution:
         kb_rows = {1:set("qwertyuiop"),2:set("asdfghjkl"),3:set("zxcvbnm")}
         result =[]
         for word in words:
+            lower_word = word.lower()
             for key,val in kb_rows.items():
-                if all(char in val for char in word.lower()):
+                if all(char in val for char in lower_word):
                     result.append(word)
                     break
         return result
