304375a2abae67e22551e340da12c11b1e641345
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-16 14:59:00 -0400

    Time: 34 ms (63.29%), Space: 16.6 MB (8.97%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -5,15 +5,10 @@ class Solution:
         for char in s:
             if char == 'A':
                 count_absence+=1
-                if count_absence>1:
-                    return False
             if char == 'L':
                     cons_late +=1
-                    if cons_late>2:
-                        return False
             else:
                 cons_late = 0
+            if count_absence>1 or cons_late>2:
+                return False
         return True
-
-
-            
