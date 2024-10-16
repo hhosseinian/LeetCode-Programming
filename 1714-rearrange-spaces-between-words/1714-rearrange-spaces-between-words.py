1deda2d3c44ebb1369fe2e1fcdde11d276db81b1
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-16 15:35:20 -0400

    Time: 30 ms (83.63%), Space: 16.6 MB (15.25%) - LeetHub

--- 1714-rearrange-spaces-between-words/1714-rearrange-spaces-between-words.py
+++ 1714-rearrange-spaces-between-words/1714-rearrange-spaces-between-words.py
@@ -1,6 +1,5 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
-        N = len(text)
         N_spaces = text.count(' ')
         words =text.split()
         M = len(words)
@@ -11,8 +10,6 @@ class Solution:
             return words[0]+ ' '*N_spaces
         Equal_Spaces = ' '*(N_spaces//(M-1))
         remaining_Spaces = ' '*(N_spaces%(M-1))
-        for i in range(M-1):
-            result+= words[i]+Equal_Spaces
         result=Equal_Spaces.join(words)+remaining_Spaces
         return result
 
