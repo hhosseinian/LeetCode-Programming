d5f4185806607b548787e1fac568234447b65a97
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-15 23:33:06 -0400

    Time: 42 ms (19.68%), Space: 16.7 MB (28.94%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,15 +1,8 @@
+from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        N = len(s)
         result = 0
-        if N == 1:
-            return 1
-        char_freq ={}
-        for char in s:
-            if char not in char_freq:
-                char_freq[char] =1
-            else:
-                char_freq[char] +=1
+        char_freq = Counter(s)
         has_odd = False
         for key,val in char_freq.items():
             if val%2 == 0:
