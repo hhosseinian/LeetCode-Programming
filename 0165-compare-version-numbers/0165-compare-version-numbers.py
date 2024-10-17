425571b24dc42ba3d020119dad9fb9494653f6c9
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-16 21:22:45 -0400

    Time: 29 ms (88.65%), Space: 16.5 MB (44.72%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,18 +1,23 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        digits1 = list(map(int, version1.split('.')))
-        digits2 = list(map(int, version2.split('.')))
-        n1 = len(digits1)
-        n2 = len(digits2)
-        max_lenghs = max(n1,n2)
-        i=0
-        for i in range(max_lenghs):
-            v1 = digits1[i] if i<n1 else 0
-            v2 = digits2[i] if i<n2 else 0
+        n1 = len(version1)
+        n2 = len(version2)
+        ptr1,ptr2 =0,0
+        while ptr1<n1 or ptr1<n2:
+            segment1 = ''
+            while ptr1<n1 and version1[ptr1]!='.':
+                segment1+= version1[ptr1]
+                ptr1+=1
+            segment2 = ''
+            while ptr2<n2 and version2[ptr2]!='.':
+                segment2+= version2[ptr2]
+                ptr2+=1
+            v1 =int(segment1) if segment1 else 0
+            v2 =int(segment2) if segment2 else 0
             if v1<v2:
                 return -1
             elif v1>v2:
                 return 1
-            i+=1
+            ptr1+=1
+            ptr2+=1
         return 0
-
