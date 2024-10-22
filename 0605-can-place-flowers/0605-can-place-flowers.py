0fe73f607fc59c4f3cbc62c00dd8877632e319d7
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-21 21:44:32 -0400

    Time: 6 ms (99.03%), Space: 17 MB (34.69%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -9,4 +9,6 @@ class Solution:
             if flowerbed[i] == 0 and(i == 0 or flowerbed[i-1] == 0)and(i == m-1 or flowerbed[i+1] == 0):
                 flowerbed[i] = 1
                 n-=1
+            if n<=0:
+                return True
         return n<=0
