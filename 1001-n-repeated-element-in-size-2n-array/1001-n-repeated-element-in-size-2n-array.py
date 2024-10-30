a50400d6dd3ff926f1ec6bf67ce870fd5c12577b
Hamid Hosseinianfar <hh9af@virginia.edu>
2024-10-30 19:02:00 +0330

    Time: 0 ms (100%), Space: 17.7 MB (93.45%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,9 +1,13 @@
 from collections import Counter
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        Freq_dict = Counter(nums)
-        max_value = max(Freq_dict.values())
-        for k, v in Freq_dict.items():
-            if v == max_value:
-                keys_with_max_value = k
-        return keys_with_max_value
+        Freq_dict = {}
+        for n in nums:
+            if n in Freq_dict:
+                Freq_dict[n] += 1
+            else:
+                Freq_dict[n] = 1
+            if Freq_dict[n]>1:
+                return n
+        return -1
+
