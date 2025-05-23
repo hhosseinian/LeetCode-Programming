0aa658b79f6648092d1ee90ef49c383ecba5e35b
Hamid <hh9af@virginia.edu>
2025-05-22 22:58:10 -0400

    Time: 0 ms (100%), Space: 18.6 MB (26.06%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,10 +3,12 @@ class Solution:
         left,right = 0,len(nums)-1
         while left<=right:
             mid =(left+right)//2
+            print(left,right,mid)
             if nums[mid] == target:
                 return mid
             elif nums[mid]>target:
                 right = mid-1
             else:
                 left = mid+1
+            print('after:',left,right,mid)
         return left
