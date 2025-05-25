3962c7baaac7b987322b66af333accfd67c14ae4
Hamid <hh9af@virginia.edu>
2025-05-25 06:13:37 -0400

    Time: 0 ms (100%), Space: 17.7 MB (52.51%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.py
@@ -2,13 +2,15 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         stack = []
         result = ''
-        toggle = True
+        #toggle = True
         for char in word:
             stack.append(char)
-            if stack and toggle and char == ch:
+            #if stack and toggle and char == ch:
+            if stack and char == ch:
                 while stack:
-                    result+=stack[-1]
-                    stack.pop()
-                toggle = False
-        result+=''.join(stack)
+                    result+=stack.pop()
+                #toggle = False
+                break
+        i = len(result)
+        result+=''.join(word[i:])
         return result
